9730105966e95a25d42bd023a0337be890825fea
Danilo Camilo <danilocamilo1@hotmail.com>
2023-07-12 17:21:28 -0300

    created all the first logic, constructors, getters and setters and simple add ou remove functions

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,52 @@
 package entities;
 
-public class Order {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import entities.enums.OrderStatus;
+
+public class Order {
+	private Date moment;
+	private OrderStatus status;
+	
+	private List<OrderItem> orders = new ArrayList<>();
+	
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status) {
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	public List<OrderItem> getOrder() {
+		return orders;
+	}
+	
+	public void addItem (OrderItem order) {
+		orders.add(order);
+	}
+	
+	public void removeItem (OrderItem order) {
+		orders.remove(order);
+	}
+	
+	
 }
