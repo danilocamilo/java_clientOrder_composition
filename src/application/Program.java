ac780eede1b06d4f690cc51dddc54148868a45b6
Danilo Camilo <danilocamilo1@hotmail.com>
2023-07-14 13:18:12 -0300

    Add instances for the classes, remains to do the final summary

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,66 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdfSummary = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date now = new Date();
+
+		System.out.println("Enter client data: ");
+		System.out.print("Name: ");
+		String name = sc.next();
 		
+		System.out.print("Email: ");
+		String email = sc.next();
+		
+		System.out.print("Birth date(DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+		
+		//instance new client for print
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("\nEnter order data: ");
+		System.out.print("Status: ");
+		String orderStatus = sc.next();
+		
+		Order order = new Order(now, OrderStatus.valueOf(orderStatus));
+		
+		System.out.print("How many items for this order? ");
+		int qtItems = sc.nextInt();
+		
+		for (int i = 1; i <= qtItems; i++) {
+			System.out.println("Enter #" + i + " item data:");
+			System.out.print("Product name: ");
+			String pdName = sc.next();
+			
+			System.out.print("Product price: ");
+			double pdPrice = sc.nextDouble();
+						
+			System.out.print("Quantity");
+			int pdQuantity = sc.nextInt();
+			
+			OrderItem item = new OrderItem(pdQuantity, pdPrice);
+			
+			order.addItem(item);
+		}
+
+		sc.close();
 	}
 
 }
