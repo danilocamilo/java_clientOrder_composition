35b67db6865eca31eada2fad9b317d3f99ea9e99
Danilo Camilo <danilocamilo1@hotmail.com>
2023-07-15 11:54:21 -0300

    update OrderItem class

--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -4,6 +4,8 @@ public class OrderItem {
 	private Integer quantity;
 	private Double price;
 	
+	private Product product;
+	
 	public OrderItem() {
 	}
 
@@ -28,8 +30,31 @@ public class OrderItem {
 		this.price = price;
 	}
 	
+	
+	
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
 	public Double subTotal() {
 		return quantity * price;
 	}
+
+	@Override
+	public String toString() {
+		return getProduct().getName()
+			+ ", $"
+			+ String.format("%.2f", price)
+			+ ", Quantity: "
+			+ quantity
+			+ ", Subtotal: $"
+			+ String.format(".2f", subTotal());
+
+	}
+	
 	
 }
